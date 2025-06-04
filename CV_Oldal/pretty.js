b89b808d4dc536efa32e55b3ddc90394fd212a2c
Minya Ákos <71983910+Necromoon@users.noreply.github.com>
2025-06-04 21:39:12 +0200

    Delete CV_Oldal directory

--- CV_Oldal/pretty.js
+++
@@ -1,9 +0,0 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
-  });
-  
-  document.querySelectorAll('.about-img, .university-img, .gmail-img, .stack-list').forEach(el => observer.observe(el));
